ad41c0796c3e83f21b779fa11d02aade6eccac36
Shane <innon.shen@gmail.com>
2023-12-23 23:16:05 +0800

    sort

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rea1shane/algorithm
+
+go 1.20
---
+++ sort_test.go
@@ -0,0 +1,13 @@
+package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+var arr = []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
+
+func TestBubbleSort(t *testing.T) {
+	BubbleSort(arr)
+	fmt.Println(arr)
+}
